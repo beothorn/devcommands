fbaf528fa574b2b2a640f4ab8609a94ddda8aefd
Lucas <lucass@gmail.com>
2016-12-15 21:15:40 -0200

    moar comments

--- commands.js
+++ commands.js
@@ -1,4 +1,10 @@
 devcmd = {
+    /***
+    * Outputs a parameter string from an input json
+    * ex: {"foo":"bar","baz":"qux"}
+    * returns "foo=bar&baz=qux"
+    * obj a json
+    ***/
     toParams:function(obj) {
         var asString = "";
         var pairs = [];
@@ -8,7 +14,19 @@ devcmd = {
         return pairs.join('&');
     },
     /***
-    * postConfig {url,parameters,headers}
+    * Run a ajax post request
+    * ex:
+    * devcmd.post({
+    *   url:'/url/to/do/something.php',
+    *   parameter:{
+    *     'token':'foo',
+    *     'myValue':'42'
+    *   }
+    * }, devcmd.outputHtml)
+    * 
+    * postConfig a json with {url,parameters(optional),headers(optional)}
+    * successCallback called when return status is OK
+    * failureCallback called on any other status
     ***/
     post:function(postConfig, successCallback,failureCallback){
         var http = new XMLHttpRequest();
@@ -42,6 +60,11 @@ devcmd = {
         }
         http.send(params);
     },
+    /***
+    * Outputs html on a Iframe
+    * html_string
+    * iframeId (optional)
+    ***/
     outputHtml:function(html_string,iframeId){
         var id = id || 'notimportant';
         if (document.getElementById(id)){
@@ -53,6 +76,10 @@ devcmd = {
         document.body.appendChild(elemtIFrame);
         elemtIFrame.src = "data:text/html;charset=utf-8," + escape(html_string);
     },
+    /***
+    * Returns an array with all possible values for a select
+    * query A query that returns the select element
+    ***/
     allValuesForSelect:function(query){
         var sel = document.querySelector(query);
         var allVals = [];
@@ -61,6 +88,13 @@ devcmd = {
         }
         return allVals;
     },
+    /***
+    * Combine N arrays as a flat array with all combinations
+    * ex
+    * devcmd.combine([1,2],[2,3],[3,4])
+    * outputs
+    * "[[1,2,3],[1,2,4],[1,3,3],[1,3,4],[2,2,3],[2,2,4],[2,3,3],[2,3,4]]"
+    ***/
     combine:function(){
         if(arguments.length == 1){
             return arguments[0];
@@ -76,6 +110,9 @@ devcmd = {
             return combs;
         }
     },
+    /***
+    * Removes a element
+    ***/
     removeElement:function(query){
         var element = document.querySelector(query);
         element.outerHTML = "";
