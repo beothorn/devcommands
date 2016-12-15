672d071f2b5c3ef7f89854705d1dc1678104c08d
Lucas <lucass@gmail.com>
2016-12-15 21:02:34 -0200

    moar commands

--- commands.js
+++ commands.js
@@ -41,5 +41,44 @@ devcmd = {
             }
         }
         http.send(params);
+    },
+    outputHtml:function(html_string,iframeId){
+        var id = id || 'notimportant';
+        if (document.getElementById(id)){
+            devcmd.removeElement('#'+id);
+        }
+        var elemtIFrame = document.createElement('iframe');
+        elemtIFrame.style.cssText = 'position:absolute;width:100%;height:100%;opacity:0.3;z-index:100;';
+        elemtIFrame.id = id;
+        document.body.appendChild(elemtIFrame);
+        elemtIFrame.src = "data:text/html;charset=utf-8," + escape(html_string);
+    },
+    allValuesForSelect:function(query){
+        var sel = document.querySelector(query);
+        var allVals = [];
+        for (var i = 0; i < sel.options.length; i++) {
+            allVals[i] = sel.options[i].value;
+        }
+        return allVals;
+    },
+    combine:function(){
+        if(arguments.length == 1){
+            return arguments[0];
+        }else{
+            var combs = [];
+            var followingCombinations = devcmd.combine.apply(this, Array.prototype.slice.call(arguments, 1));
+            var values = arguments[0];
+            for(var i=0; i<values.length;i++){
+                for(var j=0; j<followingCombinations.length;j++){
+                    combs.push([values[i]].concat(followingCombinations[j]));
+                }
+            }
+            return combs;
+        }
+    },
+    removeElement:function(query){
+        var element = document.querySelector(query);
+        element.outerHTML = "";
+        delete element;
     }
 };
